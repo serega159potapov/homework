ee04ccc38d94ae58ca72491dc417c5ad2f1e3e73
serega159potapov <potapovsieriogha@gmail.com>
2025-01-19 14:22:46 +0500

    Delete lesson-79 directory

--- lesson-79/main.js
+++
@@ -1,8 +0,0 @@
-const age = 31
-if(age >= 18) {
-    console.log('Вы совершеннолетний')
-}else{
-    console.log('Вам еще нет 18')
-}
-
-age >= 18 ? console.log('Вы совершеннолетний'): console.log('Вам еще нет 18')
