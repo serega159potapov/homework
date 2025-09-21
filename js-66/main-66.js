6bacc6fec342880caaf4f12fc6312b63a42be4d3
serega159potapov <potapovsieriogha@gmail.com>
2025-09-21 16:11:15 +0500

    Add files via upload

---
+++ js-66/main-66.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const todoKeys = {
+  id: "id",
+  text: "text",
+  is_completed: "is_completed",
+  date: "date",
+};
+
+const todos = [];
+
+const errTodoNotFound = todoId => `Todo with id ${todoId} not found`;
+
+const getNewTodoId = todos =>
+  todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+
+const createTodo = (todos, { text, date }) => {
+  const newTodo = {
+    [todoKeys.id]: getNewTodoId(todos),
+    [todoKeys.text]: text,
+    [todoKeys.is_completed]: false,
+    [todoKeys.date]: date || "",
+  };
+  todos.push(newTodo);
+  return newTodo;
+};
+
+const completeTodoById = (todos, todoId) => {
+  const todo = todos.find(todo => todo[todoKeys.id] === todoId);
+
+  if (!todo) {
+    console.error(errTodoNotFound(todoId));
+    return null;
+  }
+  todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+  return todo;
+};
+
+const deleteTodoById = (todos, todoId) => {
+  const todoIndex = todos.findIndex(todo => todo[todoKeys.id] === todoId);
+  if (todoIndex === -1) {
+    console.error(errTodoNotFound(todoId));
+    return todos;
+  }
+  todos.splice(todoIndex, 1);
+  return todos;
+};
+
+const editTodoById = (todos, todoId, newText) => {
+  const todo = todos.find(todo => todo[todoKeys.id] === todoId);
+
+  if (!todo) {
+    console.error(errTodoNotFound(todoId));
+    return null;
+  }
+
+  todo[todoKeys.text] = newText;
+  return todo;
+};
+
